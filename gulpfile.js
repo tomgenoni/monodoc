9513e5fa8f1ab78d64874c7b7030c81407a0a123
Tom Genoni <tom@atomeye.com>
2017-03-10 18:11:20 -0800

    Commenting, cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
 var gulp        = require('gulp'),
     markdown    = require('gulp-markdown'),
-    insert      = require('gulp-insert'),
     marked      = require('marked'),
+    insert      = require('gulp-insert'),
     fs          = require('fs'),
     del         = require('del'),
     wrap        = require('gulp-wrap'),
     sass        = require('gulp-sass'),
     rename      = require('gulp-rename'),
     concat_json = require('gulp-concat-json'),
-    gutil       = require('gulp-util'),
     data        = require('gulp-data');
 
 var packageTypes = [
@@ -25,7 +24,7 @@ function filepath(file) {
 }
 
 // Initial step in building the JSON for use by the root index.html
-// Builds temporary JSON files
+// Builds temporary JSON files that are consumed and discared in next step.
 gulp.task('build:typeJSON', function () {
     packageTypes.forEach(function(type, index) {
         return gulp.src(['./src/' + type + '/**/package.json'])
@@ -35,9 +34,48 @@ gulp.task('build:typeJSON', function () {
     });
 });
 
+// Combine the JSON files into a single json file for use by index.json
 gulp.task('build:indexJSON', function () {
     return gulp.src(['./tmp/*.json'])
         .pipe(concat_json('index.json'))
+        .pipe(gulp.dest('dist'));
+});
+
+// Builds the documentation from all the readme files files in the source.
+gulp.task('build:packages', function () {
+    return gulp.src(['./src/**/readme.md'])
+        // Append the changelog to the readme.
+        .pipe(insert.append(function(file){
+            return fs.readFileSync(filepath(file) + 'changelog.md', 'utf8');
+        }))
+        // Pull the date from the package.json file for use by 'gulp-wrap'
+        .pipe(data(function(file) {
+            return {
+                'packageJSON': require( filepath(file) + 'package.json' )
+            }
+        }))
+        .pipe(markdown())
+        .pipe(wrap({
+            src: './template/package.tpl'
+        }))
+        // Change the file to index.html
+        .pipe(rename(function(path) {
+            path.basename = "index"
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+// Builds the CSS required by that package.
+// TODO: will need to read package.json to include any dependencies on the fly.
+gulp.task('build:packages:sass', function () {
+    return gulp.src(['./src/**/*.scss'])
+        .pipe(insert.prepend(function(file){
+            return fs.readFileSync('./assets/core.scss', 'utf8');
+        }))
+        .pipe(rename(function(path) {
+            path.basename = "index"
+        }))
+        .pipe(sass())
         .pipe(gulp.dest('dist'));
 });
 
@@ -52,37 +90,5 @@ gulp.task('copy:assets', function() {
 
 gulp.task('copy:index', function() {
     return gulp.src('./template/index.html')
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('build:packages', function () {
-    return gulp.src(['./src/**/readme.md'])
-        .pipe(insert.append(function(file){
-            return fs.readFileSync(filepath(file) + 'changelog.md', 'utf8');
-        }))
-        .pipe(data(function(file) {
-            return {
-                'packageJSON': require( filepath(file) + 'package.json' )
-            }
-        }))
-        .pipe(markdown())
-        .pipe(wrap({
-            src: './template/package.tpl'
-        }))
-        .pipe(rename(function(path) {
-            path.basename = "index"
-        }))
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('build:packages:sass', function () {
-    return gulp.src(['./src/**/*.scss'])
-        .pipe(insert.prepend(function(file){
-            return fs.readFileSync('./assets/core.scss', 'utf8');
-        }))
-        .pipe(rename(function(path) {
-            path.basename = "index"
-        }))
-        .pipe(sass())
         .pipe(gulp.dest('dist'));
 });
